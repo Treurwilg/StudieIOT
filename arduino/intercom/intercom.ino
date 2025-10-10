d3f74da8674e4b8cc8a57c1052fb90e057623b23
Jan Kooijman <jan@ict4us.nl>
2025-10-10 10:30:21 +0200

    Rpi10A: voeg setBuzzer functie toe en verbeter functie setLcd

--- arduino/intercom/intercom.ino
+++ arduino/intercom/intercom.ino
@@ -19,6 +19,9 @@ const int LCD_D4_PIN = 2;
 const int LCD_D5_PIN = 3;
 const int LCD_D6_PIN = 4;
 const int LCD_D7_PIN = 5;
+// voor de buzzer  
+const int BUZZER_PIN = 8;
+
 
 LiquidCrystal lcd(LCD_RS_PIN, LCD_E_PIN, LCD_D4_PIN,
                   LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
@@ -43,24 +46,42 @@ void setLed(String color) {
     // do nothing
   }
 }
-// format command is: "position:line:text"
+
+/* format command is: "position:line:text"
+ * position is de positie op de regel van 0 t/m 15
+ * line is de regel: 0 of 1
+ * Uitgangspunt is dat als regel 0 aangepast wordt ook regel 1 
+ * aangepast zal worden, dus worden het clear() bij regel 0 toegepast. 
+ */
 void setLCD(String command) {
   int firstLocation = command.indexOf(":");
+  int lastLocation = command.lastIndexOf(":");   
   String position = command.substring(0, firstLocation);
-  int lastLocation = command.lastIndexOf(":"); 
   String line = command.substring(firstLocation + 1, lastLocation);
   String text = command.substring(lastLocation + 1, command.length());
-  Serial.println("positie: " + position);
-  Serial.println("line: " + line);
-  Serial.println("text: " + text);
-  lcd.clear();
+  if (line == "0") {
+    lcd.clear();
+  }
   lcd.setCursor(position.toInt(),line.toInt());
   lcd.print(text);
+}
+
+/*
+ * Format command is: "frequncy:duration"
+ */
+void setBuzzer(String command) {
+  int location = command.indexOf(":"); 
+  String frequency = command.substring(0, location);
+  String duration = command.substring(location + 1, command.length());    
+  Serial.println(frequency + "  " + duration);  
+  tone(BUZZER_PIN, frequency.toInt(), duration.toInt());
 }
 
 void setup() {
   // Algemeen
   Serial.begin(115200);
+  // voor de buzzer
+  pinMode(BUZZER_PIN, OUTPUT);
   // Voor de leds
   pinMode(RGB_RED_PIN, OUTPUT);
   pinMode(RGB_YELLOW_PIN, OUTPUT);
@@ -69,6 +90,7 @@ void setup() {
   // Voor de LCD
   lcd.begin(16, 2);
   lcd.clear();
+  
 
   /* initieer hier een initiele presentatie van het systeem door 
    *  - de naam van de sketch op de LCD weer te geven 
@@ -79,10 +101,9 @@ void setup() {
 }
 
 void loop() {
-
   delay(10);
   if (Serial.available() > 0) {
-    String cmd = Serial.readStringUntil('/n');
+    String cmd = Serial.readStringUntil('\n');
     cmd.trim();
     if (cmd.startsWith("led:")) {
       cmd.remove(0, 4);
@@ -90,6 +111,11 @@ void loop() {
     } else if (cmd.startsWith("lcd:")) {
       cmd.remove(0, 4);
       setLCD(cmd);
+    } else if (cmd.startsWith("buzz:")) {
+      cmd.remove(0, 5);
+      setBuzzer(cmd);
+    } else {
+      // do nothing
     }
   }
 }
